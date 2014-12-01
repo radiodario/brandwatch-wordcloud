c518a296f3b44f145387153b28ed7d869abe6946
radiodario <dario.villanueva@gmail.com>
2014-12-01 16:54:28 +0000

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    
+    clean: ['dist'],
+
+    ejs: {
+      all: {
+        options: {
+          // site-wide vars here
+        },
+        src: ['**/*.ejs', '!node_modules/**/*', '!_*/**/*'],
+        dest: 'dist/',
+        expand: true,
+        ext: '.html',
+      },
+    },
+
+    copy: {
+      all: {
+        src: ['*.css', '*.html', 'images/**/*', 'img/**/*', '!Gruntfile.js'],
+        dest: 'dist/',
+      },
+    },
+
+    browserify: {
+      all: {
+        src: 'src/**/*.js',
+        dest: 'dist/app.js'
+      },
+      options: {
+        transform: ['debowerify']
+      }
+    },
+
+    less: {
+      all: {
+        src: 'styles/**/*.less',
+        dest: 'dist/style.css'
+      }
+    },
+
+    connect: {
+      options: {
+        port: process.env.PORT || 3131,
+        base: 'dist/',
+      },
+
+      all: {},
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+
+      html: {
+        files: '<%= ejs.all.src %>',
+        tasks: ['ejs'],
+      },
+
+      js: {
+        files: '<%= browserify.all.src %>',
+        tasks: ['browserify'],
+      },
+
+      less: {
+        files: '<%= less.all.src %>',
+        tasks: ['less']
+      },
+
+      assets: {
+        files: ['assets/**/*', '*.css', 'images/**/*', 'img/**/*', '!Gruntfile.js'],
+        tasks: ['copy'],
+      }
+    },
+
+    'gh-pages': {
+      options: {
+        base: 'dist/'
+      },
+      src: ['**/*']
+    }
+  });
+
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  
+  grunt.registerTask('default', ['clean', 'ejs', 'less', 'browserify', 'copy']);
+  
+  grunt.registerTask('server', ['default', 'connect', 'watch']);
+
+  grunt.registerTask('deploy', ['default', 'gh-pages']);
+
+};
---
+++ readme.md
@@ -0,0 +1,25 @@
+# brandwatch-wordcloud
+> A word cloud generated for brandwatch's coding test
+
+## grunt-static-site usage:
+Initialize the git repository for the project:
+```
+git init
+```
+
+Add and commit the files:
+```
+git add .
+git commit -m 'initial commit'
+```
+
+[Create a new repository on github](http://github.com/new)
+
+Make sure it has the same name you entered when you generated this project: brandwatch-wordcloud
+
+Edit the project files to add your content.
+
+Deploy to GitHub Pages:
+```
+grunt deploy
+```
